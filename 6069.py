e8e8bbede16898f56061dfd1bd3f1ed382475556
donghyun <donghyun.ict@gmail.com>
2021-09-18 15:53:56 +0900

    평가 입력받아 다르게 출력하기

---
+++ 6069.py
@@ -0,0 +1,12 @@
+e = input()
+
+if (e == 'A'):
+    print('best!!!')
+elif (e == 'B'):
+    print('good!!')
+elif (e == 'C'):
+    print('run!')
+elif (e == 'D'):
+    print('slowly~')
+else:
+    print('what?')
